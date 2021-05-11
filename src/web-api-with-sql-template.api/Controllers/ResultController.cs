b1ffb1501116e156c16cea529be6270c9dead5dd
propes <>
2021-05-11 15:52:29 +0100

    Add endpoint stub scenarios

--- src/web-api-with-sql-template.api/Controllers/ResultController.cs
+++ src/web-api-with-sql-template.api/Controllers/ResultController.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiWithSqlTemplate.Domain.Results;
 
@@ -6,6 +9,18 @@ namespace WebApiWithSqlTemplate.Api.Controllers
 {
     public abstract class ResultController : ControllerBase
     {
+        private readonly IDictionary<Guid, Func<Task<ActionResult>>> _stubScenarios;
+        
+        protected ResultController()
+        {
+            _stubScenarios = new Dictionary<Guid, Func<Task<ActionResult>>>
+            {
+                {Guid.Parse("12345678-abc1-def2-0429-123456789abc"), RateLimited},
+                {Guid.Parse("12345678-abc1-def2-0500-123456789abc"), ServerError},
+                {Guid.Parse("12345678-abc1-def2-0504-123456789abc"), Timeout}
+            };
+        }
+        
         protected ActionResult<TDto> AcceptedFromResult<T, TDto>(Result<T> result, Func<T, TDto> map)
         {
             if (!result.IsSuccess)
@@ -37,6 +52,27 @@ namespace WebApiWithSqlTemplate.Api.Controllers
             }
             
             return NotFound();
+        }
+
+        protected bool TryGetStubScenario(Guid id, out Func<Task<ActionResult>> stubScenario) =>
+            _stubScenarios.TryGetValue(id, out stubScenario);
+
+        private async Task<ActionResult> RateLimited()
+        {
+            Response.Headers.Add("Retry-After", "60");
+
+            return await Task.FromResult(StatusCode(StatusCodes.Status429TooManyRequests));
+        }
+
+        private async Task<ActionResult> ServerError()
+        {
+            return await Task.FromResult(StatusCode(StatusCodes.Status500InternalServerError));
+        }
+
+        private async Task<ActionResult> Timeout()
+        {
+            await Task.Delay(TimeSpan.FromMinutes(2));
+            return StatusCode(StatusCodes.Status504GatewayTimeout);
         }
     }
 }
--- src/web-api-with-sql-template.api/Controllers/TodoListController.cs
+++ src/web-api-with-sql-template.api/Controllers/TodoListController.cs
@@ -18,12 +18,27 @@ namespace WebApiWithSqlTemplate.Api.Controllers
         {
             _handler = handler;
         }
-        
+
         /// <summary>
         /// Gets a todo list by id.
         /// </summary>
         /// <param name="id">The todo list id.</param>
         /// <returns>The todo list or an error code.</returns>
+        /// <remarks>
+        /// Stub responses:
+        ///
+        ///     GET /apis/customers/registration/v1/registration/12345678-abc1-def2-0429-123456789abc
+        ///
+        /// Returns a rate limited response.
+        ///
+        ///     GET /apis/customers/registration/v1/registration/12345678-abc1-def2-0500-123456789abc
+        ///
+        /// Returns a server error.
+        /// 
+        ///     GET /apis/customers/registration/v1/registration/12345678-abc1-def2-0504-123456789abc
+        ///
+        /// Blocks for 2 minutes, resulting in a timeout.
+        /// </remarks>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -35,8 +50,11 @@ namespace WebApiWithSqlTemplate.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<TodoListDto>> Get(Guid id)
         {
-            // TODO: add stub scenarios.
-            
+            if (TryGetStubScenario(id, out var stubScenario))
+            {
+                return await stubScenario();
+            }
+
             var result = await _handler.Handle(new GetTodoList
             {
                 Id = id
